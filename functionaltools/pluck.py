1fe2c8746b851a2760c6d662648c7c13e190b466
Rodgers, Lewis <lewis.rodgers@accenture.com>
2019-11-25 09:21:32 -0500

    Refactor

--- functionaltools/pluck.py
+++ functionaltools/pluck.py
@@ -15,3 +15,16 @@ def pluck(p, xs):
         list: The list of values for the given property.
     """
     return map(prop(p), xs)
+
+# @curry
+# def pluck(p, itr):
+#     """Returns a new list by plucking the same named property off all objects in the list supplied.
+
+#     Args:
+#         p (str): The property name to pluck off of each object.
+#         itr (:obj:`iter` of :obj:`dict`): The list to consider.
+
+#     Returns:
+#         iter: The list of values for the given property.
+#     """
+#     return (dct.get(p) for dct in itr)
--- functionaltools/tap.py
+++ functionaltools/tap.py
@@ -1,3 +1,11 @@
-def tap(x):
-    print(">>> {}\n---".format(x))
-    return x
+from .curry import curry
+
+
+def tap(f=None):
+    def tap_(x):
+        if f is not None:
+            f(x)
+        else:
+            print(">>> {}\n---".format(x))
+        return x
+    return tap_
